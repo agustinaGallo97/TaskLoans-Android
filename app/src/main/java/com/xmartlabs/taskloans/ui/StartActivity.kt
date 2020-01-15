3ef2372f48dfc450277359808adfd3869e1d8919
Agustina <agustina@xmartlabs.com>
2020-01-15 13:59:15 -0300

    more suggestions added

--- app/src/main/java/com/xmartlabs/taskloans/ui/StartActivity.kt
+++ app/src/main/java/com/xmartlabs/taskloans/ui/StartActivity.kt
@@ -15,16 +15,16 @@ class StartActivity : BaseAppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-//    val session = sessionRepository.session
-//    if (session == null) {
+    val session = sessionRepository.session
+    if (session == null) {
 //      // TODO: handle on start activity no session
     val intent = Henson.with(context)
         .gotoSignInActivity()
         .build()
         .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION or Intent.FLAG_ACTIVITY_CLEAR_TASK)
     startActivity(intent)
-//    } else {
-//      TODO("Handle on start activity with session")
-//    }
+    } else {
+      TODO("Handle on start activity with session")
+    }
   }
 }
